0afb37ae12ce5a84adbf9811cd3a4bb7e781f405
Aryan raj arya <161276410+Sameal11@users.noreply.github.com>
2025-07-09 04:52:49 +0530

    Update firebase-config.js

--- Attendance-web/firebase/firebase-config.js
+++ Attendance-web/firebase/firebase-config.js
@@ -3,13 +3,13 @@
 
 // Firebase config object
 const firebaseConfig = {
-  apiKey: "AIzaSyAmO5uYhlv4SebEV3zAV_QeUQ5cuh0fQhE",
-  authDomain: "studaa-1d2d1.firebaseapp.com",
-  databaseURL: "https://studaa-1d2d1-default-rtdb.firebaseio.com",
-  projectId: "studaa-1d2d1",
-  storageBucket: "studaa-1d2d1.firebasestorage.app",
-  messagingSenderId: "158278697705",
-  appId: "1:158278697705:web:dff36a193ad5b39af36678",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
   measurementId: "G-Z9DEW9NZT6"
 };
 
